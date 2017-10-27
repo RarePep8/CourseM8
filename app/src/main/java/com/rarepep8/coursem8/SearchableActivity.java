0305f7944130da25a10e753c6aa8090e4ed2f5bf
Jiayi Zhao <jeff.no.zhao@gmail.com>
2017-10-26 21:27:01 -0400

    fix bug where two activities are created when search dialog is created - search dialog no longer takes user to new visible activity

--- app/src/main/java/com/rarepep8/coursem8/SearchableActivity.java
+++ app/src/main/java/com/rarepep8/coursem8/SearchableActivity.java
@@ -4,21 +4,16 @@ import android.app.Activity;
 import android.app.SearchManager;
 import android.content.Intent;
 import android.os.AsyncTask;
-import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 import org.json.JSONTokener;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.ArrayList;
 
 import javax.net.ssl.HttpsURLConnection;
 
